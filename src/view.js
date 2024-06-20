1ab5031c6d2aec9ee2c0af0af64d97cb5b4d547f
BRIGIDBERRY <brigid.ms21@gmail.com>
2024-06-20 00:53:04 -0500

    hito2-visualizar-punto 2ul, 3foreach y li aun por chekar

--- src/view.js
+++ src/view.js
@@ -1,6 +1,28 @@
 export const renderItems = (data) => {
+// Aquí comienza tu código y puedes retornar lo que tu necesites
+  // creando ul
+  const crearUl = document.createElement("ul")
+
+  //cramos un for each----parece que dentro debemos crear cada li(24?)
+  data.forEach(item => { //ITEM representa cada iteracion-funcion del arreglo
+    const crearLi = document.createElement("li")// creamos un li
+    crearLi.textConten = item;
+    crearUl.appendChild(crearLi)
+    console.log(item);
+
+
+
+  });
+
+
+
+  
+  
+
+
+
   console.log(data)
-  // Aquí comienza tu código y puedes retornar lo que tu necesites
+
   return 'example';
 };
 
