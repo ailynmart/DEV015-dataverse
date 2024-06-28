c350f1a403a48f537cdd228ec1a48858d629d043
BRIGIDBERRY <brigid.ms21@gmail.com>
2024-06-28 16:50:54 -0500

    en html id son = for,en main al parecer ya pude hacer un eveto comente los otros cont porque cuando queria ver el test solo me saleerror y me sigue saliendo eso apezar que comente,pero en la consola del servidor si se puede ver ,en datafuntion añadi lo que pedia el hito lo de ordenar y la parte 5 del readme

--- src/dataFunctions.js
+++ src/dataFunctions.js
@@ -1,9 +1,14 @@
 // Estas funciones son ejemplos, aquí puedes desarrollar tus propias funciones.
 
-export const example = () => {
-  return 'example';
+export const filterData = (data, filterBy, value) => { //H3-1.3 
+  return 'filterData';
 };
 
-export const anotherExample = () => {
+export const sortData = (data, sortBy, sortOrder) => { // se caMbia ANOTHERNXAMPLE POR SORTBY
   return [];
 };
+
+export const computeStats = (data) => { // se AÑANDE COMPUTESTATS readme-datafuntions-5.3 OJO segun creo LAS EXPORT debe tener su lugar de IMPORT(main) (h3.2-3,4)
+  return [];
+};
+
