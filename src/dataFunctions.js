d7ea4f2da9f8d884524b5bc0a79070b6305d3e61
Ailyn Martinez <ailyn.serrano09@gmail.com>
2024-07-03 11:32:04 -0600

    Modifique todo quitando lo que comentamos de codigo que ya no vamos a utilizar, aun falta seguir corrigiendo lo de dataset, html, js que no pasamos en el test

--- src/dataFunctions.js
+++ src/dataFunctions.js
@@ -13,18 +13,3 @@ export const sortData = (data, property, ascending = true) => {
 };
 
 
-/* Estas funciones son ejemplos, aquí puedes desarrollar tus propias funciones.
-
-// eslint-disable-next-line no-unused-vars
-export const filterData = (data, filterBy, value) => { //H3-1.3 
-  return 'filterData';
-};
-
-export const sortData = (data, sortBy, sortOrder) => { // se caMbia ANOTHERNXAMPLE POR SORTBY
-  return [];
-};
-
-export const computeStats = (_data) => { // se AÑANDE COMPUTESTATS readme-datafuntions-5.3 OJO segun creo LAS EXPORT debe tener su lugar de IMPORT(main) (h3.2-3,4)
-  return [];
-};
-*/
